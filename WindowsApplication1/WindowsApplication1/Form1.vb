c068f2400cdef700d25ed37a0d9775a371a9b9bf
Administrator@MYDOMAIN.LK <Administrator@MYDOMAIN.LK>
2019-01-01 10:53:21 -0800

    Changes

--- WindowsApplication1/WindowsApplication1/Form1.vb
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -1,6 +1,11 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("Hellow World")
+        Dim x As Integer = Integer.Parse(TextBox1.Text)
+        Dim y As Integer = Integer.Parse(TextBox2.Text)
+        Dim z As Integer = x + y
+        MessageBox.Show("Addition of " + x + "and " + y + "is :" + z)
+
     End Sub
+
 End Class
